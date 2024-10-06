4ee3611a3a1e297c04abac37d6d8718f35c191bc
a-random441 <somerandomgithubacc@gmail.com>
2024-10-06 21:45:12 +0100

    no.

--- source/meta/states/menus/MainMenuState.hx
+++ source/meta/states/menus/MainMenuState.hx
@@ -180,7 +180,7 @@ class MainMenuState extends MusicBeatState
 		#end
 
 		#if android
-	        addVirtualPad(UP_DOWN, A_B);
+	        addVirtualPad(UP_DOWN, A_B_E);
                 #end
 
 		super.create();
@@ -278,7 +278,7 @@ class MainMenuState extends MusicBeatState
 				}
 			}
 			#if desktop
-			else if (controls.MASTER #if android || FlxG.android.justReleased.BACK #end)
+			else if (controls.MASTER #if android || virtualPad.buttonE.justPressed #end)
 			{
 				selectedSomethin = true;
 			//	FlxTween.tween(FlxG.sound.music, {pitch: 0.3, volume: 0.3}, 1, {ease: FlxEase.cubeOut});
