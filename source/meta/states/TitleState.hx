47c7ee90d8ae7a48d207284ad2c8cd2e688d117f
a-random441 <somerandomgithubacc@gmail.com>
2024-10-06 18:44:28 +0100

    AHHHHHH

--- source/meta/states/TitleState.hx
+++ source/meta/states/TitleState.hx
@@ -64,7 +64,7 @@ class TitleState extends MusicBeatState
 		FlxG.android.preventDefaultKeys = [BACK];
 		#end
 			
-		#if (polymod && !html5)
+		/*#if (polymod && !html5)
 		if (sys.FileSystem.exists(SUtil.getPath() + 'mods/')) {
 			var folders:Array<String> = [];
 			for (file in sys.FileSystem.readDirectory(SUtil.getPath() + 'mods/')) {
@@ -76,7 +76,7 @@ class TitleState extends MusicBeatState
 			if(folders.length > 0) {
 				polymod.Polymod.init({modRoot: "mods", dirs: folders});
 			}
-		}
+		}*/
 
 		//Gonna finish this later, probably
 		#end
