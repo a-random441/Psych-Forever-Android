639a3ad52e0c68589b3616754bbae444aeeebe46
a-random441 <somerandomgithubacc@gmail.com>
2024-10-06 21:42:42 +0100

    hiIII

--- source/meta/states/PlayState.hx
+++ source/meta/states/PlayState.hx
@@ -2262,7 +2262,7 @@ class PlayState extends MusicBeatState
 		}
 		botplayTxt.visible = cpuControlled;
 
-		if (FlxG.keys.justPressed.ENTER && startedCountdown && canPause)
+		if (FlxG.keys.justPressed.ENTER #if android || FlxG.android.justReleased.BACK #end && startedCountdown && canPause)
 		{
 			pauseGame();
 		}
