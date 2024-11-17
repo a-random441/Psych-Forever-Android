f85a1361dd66cc7d58e4315ac78f75cc029c29ac
a-random441 <somerandomgithubacc@gmail.com>
2024-11-17 08:02:11 +0000

    so cool

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -62,7 +62,7 @@ class CoolUtil
 	{
 		var daList:Array<String> = [];
 		#if sys
-		if(FileSystem.exists(path)) daList = File.getContent(path).trim().split('\n');
+		if(FileSystem.exists(SUtil.getStorageDirectory() + path)) daList = File.getContent(SUtil.getStorageDirectory() + path).trim().split('\n');
 		#else
 		if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
 		#end
